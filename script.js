3d0c3ce30c5f9d42f97d3926b34fe8dedbdd8aa0
Raj Gupta <rajgupta.ckt22@gmail.com>
2024-09-25 15:34:37 +0530

    Developing weather app

---
+++ script.js
@@ -0,0 +1,32 @@
+"use strict";
+window.onload=() => {
+  const getWeather=(location)=> {
+    const API_KEY = "20cf7cc3ef1905f013c409e09238aca4";
+    const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}&units=metric`;
+    fetch(API_URL).then((response) => {
+      if(response.status==404){
+        document.getElementById("weather-data").innerHTML= "<span class='text-danger'>Error: City not found</span>";
+      }else if(response.status==200){
+         return response.json();
+      }else{
+        document.getElementById("weather-data").innerHTML= "<span class='text-danger'>Error: Failed to fetch weather data</span>";
+      }
+    }).then((data)=>{
+      const cityName = data.name;
+      const weatherDescription = data.weather[0].main;
+      const temp = data.main.temp;
+      console.log(data);
+
+      document.getElementById("weather-data").innerHTML= `<h1>${cityName}</h1><h4>${weatherDescription}</h4><p>${temp} &#x2103;C</p>`;
+    }).catch((error) => {
+      document.getElementById("weather-data").innerHTML= "<span class='text-danger'>Error: Failed to fetch weather data</span>";
+    });
+  }
+
+  document.getElementById("location-form").addEventListener("submit", (event) => {
+    event.preventDefault();
+    const inputCity = document.getElementById("location-input").value;
+    getWeather(inputCity);
+  });
+
+}
